b44ba8eed64a5af8e403d9435cb75f3411aa1858
tathagatamishra <tathsgatamishra007@gmail.com>
2023-11-22 16:29:55 +0530

    btn

--- src/Component/Membership/Membership.jsx
+++ src/Component/Membership/Membership.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import './Membership.scss'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Membership.scss";
 
 export default function Membership() {
   return (
-    <div className='Membership'>
-     
+    <div className="Membership">
       <section className="Hero">
-          <h1>Membership</h1>
-          <p>Shubukan India accepts membership in its certain conditions and terms. We practice Shorin Ryu and Okinawan Kobudo. We focus on practical self defence and combat, Shorin Ryu shubukan is an Okinawan traditional karate. We accepts club district and state membership. Remember, training is the strongest piller of Shubukan India school. Contact us to join Shubukan India.</p>
-          
-          <p>Membership of Shubukan India abide by rules and regulation. Please contact us for more details.</p>
+        <h1>Membership</h1>
+        <p>
+          Shubukan India accepts membership in its certain conditions and terms.
+          We practice Shorin Ryu and Okinawan Kobudo. We focus on practical self
+          defence and combat, Shorin Ryu shubukan is an Okinawan traditional
+          karate. We accepts club district and state membership. Remember,
+          training is the strongest piller of Shubukan India school. Contact us
+          to join Shubukan India.
+        </p>
 
-          
+        <p>
+          Membership of Shubukan India abide by rules and regulation. Please
+          contact us for more details.
+        </p>
+
+        <div className="contactBtn">
+        <NavLink to="/Contact">
+          <button>Get in touch</button>
+          </NavLink>
+        </div>
       </section>
     </div>
-  )
+  );
 }
--- src/Component/ShubukanOkinawa/ShubukanOkinawa.jsx
+++ src/Component/ShubukanOkinawa/ShubukanOkinawa.jsx
@@ -5,9 +5,9 @@ import { IonIcon } from "@ionic/react";
 import { logoFacebook } from "ionicons/icons";
 
 import img1 from "../../assets/shubukanDojo.jpg";
-import img4 from "../../assets/Frame 1.png";
-import img3 from "../../assets/Frame 2.png";
 import img2 from "../../assets/Frame 3.png";
+import img3 from "../../assets/Frame 2.png";
+import img4 from "../../assets/Frame 1.png";
 
 export default function ShubukanOkinawa() {
   return (
@@ -20,24 +20,37 @@ export default function ShubukanOkinawa() {
         </div>
 
         <p>
-          Shubukan was founded by Sensei Uema Joki Sensei. Now his Son Sensei
-          Uema Yasuhiro is now chairman of Okinawa Shubukan.
+          In 1933, Master Choshin Chibana named Shorin Ryu. In 1948, they
+          organized Okinawa Shorin Ryu Karate-do Association. They took over the
+          Shuri-te stream. There are Dojo not only in Okinawa but also
+          overseas. 
+          <br />
+          <br />
+          Shorin Ryu Shubukan was founded by Sensei Joki Uema. Now his
+          son Sensei Uema Yasuhiro is now chairman of Okinawa Shubukan.
         </p>
 
         <div className="family">
           <div className="child">
             <img src={img2} alt="" />
-            <p>Sensei Uema Yasuhiro <br /> (10th Dan) <br /> (DOB- 15 August 1945)</p>
+            <p>
+              Sensei Uema Yasuhiro <br /> (10th Dan) <br /> (DOB- 15 August
+              1945)
+            </p>
           </div>
           <div className="parent">
             <img src={img3} alt="" />
             <p>
-              Sensei Uema Joki <br /> (10th Dan) <br /> ( DOB- 13 June 1920 <br /> DOD- 20 July 2011 )
+              Sensei Uema Joki <br /> (10th Dan) <br /> ( DOB- 13 June 1920{" "}
+              <br /> DOD- 20 July 2011 )
             </p>
           </div>
           <div className="child">
             <img src={img4} alt="" />
-            <p>Sensei Uema Takeshi <br /> (7th Dan) <br /> (DOB- 11 February 1975)</p>
+            <p>
+              Sensei Uema Takeshi <br /> (7th Dan) <br /> (DOB- 11 February
+              1975)
+            </p>
           </div>
         </div>
 
