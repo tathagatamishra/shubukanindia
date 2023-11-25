5e0625833a73e1ccdf811c9bdc013b9244384a3f
tathagatamishra <tathsgatamishra007@gmail.com>
2023-11-25 16:37:20 +0530

    seo

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { hydrate, render } from "react-dom";
 import App from "./App";
 import "./main.scss";
 
-const renderer = ReactDOM.createRoot(document.getElementById("root"));
+// const renderer = ReactDOM.createRoot(document.getElementById("root"));
 
-renderer.render(<App />);
+// renderer.render(<App />);
+
+// const App = (<App />)
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
