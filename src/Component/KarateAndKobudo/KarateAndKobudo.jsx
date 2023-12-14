9377f3fe7010db8f367e552bc695bbd3d5d2f561
tathagatamishra <tathsgatamishra007@gmail.com>
2023-12-14 09:47:01 +0530

    kara

--- src/Component/KarateAndKobudo/KarateAndKobudo.jsx
+++ src/Component/KarateAndKobudo/KarateAndKobudo.jsx
@@ -9,7 +9,6 @@ import eku from "../../assets/eku.svg";
 import nunchaku from "../../assets/Frame 16.svg";
 
 export default function KarateAndKobudo() {
-
   useEffect(() => {
     window.scrollTo({
       top: 0,
@@ -20,36 +19,45 @@ export default function KarateAndKobudo() {
     <div className="KarateAndKobudo">
       <section className="Karate-Hero">
         <h1>Karate</h1>
-        {/* <p>Discovering the Way of Words</p> */}
+        <h2>空手</h2>
         <p>
           Karate means Empty hand. Karate makes its practitioner physically and
           mentally strong along with strong ethical and moral values. Basically
           karateka use legs and hand parts for block, strike, hold, throw etc.
-          Karate has three elements.
+          Karate has three elements - KihonDosa, Kata and Kumite.
         </p>
-        <h2>KihonDosa</h2>
-        <p>
-          Study of basics Kartateka learn block (uke), strike (uchi), kick
-          (Keri), stance (Tachi/Dachi), Punch (Tsuki). Main stances in Shorin
-          Ryu is Siko Dachi, Neko Ashi Dachi, Zenkutsu Dachi, Kosa Dachi,
-          Kokutsu Dachi etc. Blocks are mainly age uke, soto uke, uchi uke,
-          gedan uke and shuto uke etc. kicks ar Mae keri, Mawashi keri, Kansetsu
-          keri etc.
-        </p>
-        <h2>Kata</h2>
-        <p>
-          Kata is predesigned form using kihons. There are lot of katas in
-          Shubukan School. We practice above 50 katas in shubukan Shorin Ryu. In
-          our school we study Fukyugata ichi, ni, San and pinan katas, passai
-          katas, Kushanku Katas and Rohai, Wanshu etc.
-        </p>
-        <h2>Kumite</h2>
-        <p>
-          Kumite means sparring of fighting. Shubukan maintain traditional old
-          style full contact kumite. With care Shubukan focus on self defense
-          and teaches its members the true practical application of katas in
-          effective way.
-        </p>
+
+        <div className="element">
+          <h2>KihonDosa</h2>
+          <p>
+            In the study of basics Kartateka learn block (uke), strike (uchi),
+            kick (Keri), stance (Tachi/Dachi), Punch (Tsuki). Main stances in
+            Shorin Ryu is Siko Dachi, Neko Ashi Dachi, Zenkutsu Dachi, Kosa
+            Dachi, Kokutsu Dachi etc. Blocks are mainly age uke, soto uke, uchi
+            uke, gedan uke and shuto uke etc. kicks ar Mae keri, Mawashi keri,
+            Kansetsu keri etc.
+          </p>
+        </div>
+
+        <div className="element">
+          <h2>Kata</h2>
+          <p>
+            Kata is predesigned form using kihons. There are lot of katas in
+            Shubukan School. We practice above 50 katas in shubukan Shorin Ryu.
+            In our school we study Fukyugata ichi, ni, San and pinan katas,
+            passai katas, Kushanku Katas and Rohai, Wanshu etc.
+          </p>
+        </div>
+        
+        <div className="element">
+          <h2>Kumite</h2>
+          <p>
+            Kumite means sparring of fighting. Shubukan maintain traditional old
+            style full contact kumite. With care Shubukan focus on self defense
+            and teaches its members the true practical application of katas in
+            effective way.
+          </p>
+        </div>
       </section>
 
       <section className="Kobudo-Hero">
@@ -98,9 +106,7 @@ export default function KarateAndKobudo() {
           <div className="sai">
             <div className="text">
               <h2>Tonfa</h2>
-              <p>
-              It is a wooden weapon. Shubukan has many tonfa katas.
-              </p>
+              <p>It is a wooden weapon. Shubukan has many tonfa katas.</p>
             </div>
             <div className="img">
               <img src={tonfa} alt="" />
@@ -110,7 +116,10 @@ export default function KarateAndKobudo() {
           <div className="eku">
             <div className="text">
               <h2>Eku</h2>
-              <p>It is fisherman's tool used for rowing. In Okinawan kobudo it used as a excellent weapon.</p>
+              <p>
+                It is fisherman's tool used for rowing. In Okinawan kobudo it
+                used as a excellent weapon.
+              </p>
             </div>
             <div className="img">
               <img src={eku} alt="" />
@@ -120,7 +129,9 @@ export default function KarateAndKobudo() {
           <div className="nunchaku">
             <div className="text">
               <h2>Nunchaku</h2>
-              <p>It is a wooden weapon. Very important weapon of Okinawa kobudo.</p>
+              <p>
+                It is a wooden weapon. Very important weapon of Okinawa kobudo.
+              </p>
             </div>
             <div className="img">
               <img src={nunchaku} alt="" />
