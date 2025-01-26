a241debbfa28274679146b6876f62a5275930cf8
Tathagata Mishra <tathagatamishra007@gmail.com>
2025-01-26 12:08:32 +0530

    popup update

--- src/Component/UIComponent/Popup.jsx
+++ src/Component/UIComponent/Popup.jsx
@@ -1,30 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Popup.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Popup.scss";
 
 const Popup = ({ isOpen, onClose, notices }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="popup-container">
-      <button onClick={onClose}>X</button>
-      <h1>Notice</h1>
-      <div className="content">
-        {notices.map((notice, index) => (
-          <React.Fragment key={index}>
-            <hr />
-            <p>{notice}</p>
-          </React.Fragment>
-        ))}
+    <>
+      <div className="popup-container">
+        <button onClick={onClose}>X</button>
+        <h1>Notice</h1>
+        <div className="content">
+          {notices.map((notice, index) => (
+            <React.Fragment key={index}>
+              <hr />
+              <p>{notice}</p>
+            </React.Fragment>
+          ))}
+        </div>
       </div>
-    </div>
+
+      <div className="overlay" onClick={onClose}></div>
+    </>
   );
 };
 
 Popup.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   onClose: PropTypes.func.isRequired,
-  notices: PropTypes.arrayOf(PropTypes.string).isRequired
+  notices: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Popup;
