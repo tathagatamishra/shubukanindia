db4e196c196d554e784efaddeb2d3ad891fb0403
Tathagata Mishra <tathagatamishra007@gmail.com>
2025-02-05 16:26:49 +0530

    admin panel

---
+++ src/Component/Admin/Gallery/GalleryBoard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import './GalleryBoard.scss'
+
+export default function GalleryBoard() {
+  return (
+    <>
+      <label htmlFor="header">header</label>
+      <input type="text" id="header" />
+
+      <label htmlFor="subtitle">subtitle</label>
+      <textarea name="" id="subtitle"></textarea>
+
+      <label htmlFor="image">image</label>
+      <input type="text" id="image" />
+
+      <label htmlFor="title">title</label>
+      <input type="text" id="title" />
+
+      <label htmlFor="description">description</label>
+      <textarea name="" id="description"></textarea>
+
+      <label htmlFor="type">type</label>
+      <input type="text" id="type" />
+
+      <label htmlFor="time">time</label>
+      <input type="text" id="time" />
+    </>
+  );
+}
