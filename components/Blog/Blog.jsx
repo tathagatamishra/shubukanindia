66c798d57382fd3a06c0f12c02be01c210816b00
Tathagata Mishra <tathagatamishra007@gmail.com>
2025-04-04 22:43:16 +0530

    window

--- components/Blog/Blog.jsx
+++ components/Blog/Blog.jsx
@@ -4,7 +4,6 @@ import "./Blog.scss";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 
-
 export default function Blog() {
   const router = useRouter();
   const navigate = (page) => {
@@ -19,24 +18,26 @@ export default function Blog() {
   const [searchPos, setSearchPos] = useState({ marginBottom: "0" });
   const [lastScrollTop, setLastScrollTop] = useState(Infinity);
 
-  window.addEventListener("scroll", function () {
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (window !== undefined) {
+    window.addEventListener("scroll", function () {
+      let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-    if (scrollTop > lastScrollTop) {
-      setPosition({
-        height: "8rem",
-        top: "0",
-        zIndex: "5",
-        boxShadow: "inset 0px 230px 30px -150px rgb(236, 231, 226)",
-      });
-      setSearchPos({ marginBottom: "4rem" });
-    } else {
-      setPosition({ height: "4rem", top: "6rem", zIndex: "15" });
-      setSearchPos({ marginBottom: "0" });
-    }
+      if (scrollTop > lastScrollTop) {
+        setPosition({
+          height: "8rem",
+          top: "0",
+          zIndex: "5",
+          boxShadow: "inset 0px 230px 30px -150px rgb(236, 231, 226)",
+        });
+        setSearchPos({ marginBottom: "4rem" });
+      } else {
+        setPosition({ height: "4rem", top: "6rem", zIndex: "15" });
+        setSearchPos({ marginBottom: "0" });
+      }
 
-    setLastScrollTop(scrollTop);
-  });
+      setLastScrollTop(scrollTop);
+    });
+  }
 
   useEffect(() => {
     window.scrollTo({
