b9e860386300b9a20a7bc2356aa38abd7007f2cd
Tathagata Mishra <tathagatamishra007@gmail.com>
2025-04-04 22:39:46 +0530

    blog page update

---
+++ components/Blog/Blog.jsx
@@ -0,0 +1,65 @@
+"use client";
+import { useEffect, useState } from "react";
+import "./Blog.scss";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+
+
+export default function Blog() {
+  const router = useRouter();
+  const navigate = (page) => {
+    router.push(page);
+  };
+
+  const [position, setPosition] = useState({
+    height: "4rem",
+    top: "6rem",
+    zIndex: "15",
+  });
+  const [searchPos, setSearchPos] = useState({ marginBottom: "0" });
+  const [lastScrollTop, setLastScrollTop] = useState(Infinity);
+
+  window.addEventListener("scroll", function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+      setPosition({
+        height: "8rem",
+        top: "0",
+        zIndex: "5",
+        boxShadow: "inset 0px 230px 30px -150px rgb(236, 231, 226)",
+      });
+      setSearchPos({ marginBottom: "4rem" });
+    } else {
+      setPosition({ height: "4rem", top: "6rem", zIndex: "15" });
+      setSearchPos({ marginBottom: "0" });
+    }
+
+    setLastScrollTop(scrollTop);
+  });
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+    });
+  }, []);
+
+  return (
+    <div className="Blog">
+      <div className="blogPage">
+        {/* <section className="blogHero">
+          <p className="heading">Blog</p>
+          <p>The blog is under construction...</p>
+        </section> */}
+
+        <section className="blogNav">
+          <div className="opt">Top Stories</div>
+          <div className="opt">Latest</div>
+          <div className="opt">Shubukan</div>
+        </section>
+        <section className="blogContent"></section>
+        <section className="blogFooter"></section>
+      </div>
+    </div>
+  );
+}
