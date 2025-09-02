e146ea31f7219abc5b89389845effd98b17712c2
Tathagata Mishra <tathagatamishra007@gmail.com>
2025-09-03 01:03:10 +0530

    ui update

--- components/UIComponent/Banner.jsx
+++ components/UIComponent/Banner.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import "./Banner.scss";
-import { useRouter, usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export default function Banner({
   bannerText1,
@@ -22,11 +22,17 @@ export default function Banner({
   // const text4 = bannerText4 || "Register to Shubukan India.";
  
   // Under development
-  const text1 = bannerText1 || "Website is under development";
-  const text2 = bannerText2 || "Some content may change frequently";
-  const text3 =
-    bannerText3 || "Pages can be not responsive";
-  const text4 = bannerText4 || "Contact us to know more";
+  // const text1 = bannerText1 || "Website is under development";
+  // const text2 = bannerText2 || "Some content may change frequently";
+  // const text3 =
+  //   bannerText3 || "Pages can be not responsive";
+  // const text4 = bannerText4 || "Contact us to know more";
+
+  // Dojo 
+  const text1 = bannerText1 || "Contact us to know more";
+  const text2 = bannerText2 || "The Shubukan Dojo in Okinawa, the heart of Shorin-ryu Karate, has stood strong for many years, preserving tradition and shaping countless lives. Now, as the dojo ages, it needs renovation to carry its legacy forward.";
+  const text3 = bannerText3 || "Contact us to know more";
+  const text4 = bannerText4 || "On the occasion of Shubukan's 50th anniversary, we humbly request all members, students, and well-wishers to contribute generously. Your support will help rebuild the dojo and ensure that the spirit of Shubukan continues to inspire future generations.";
 
   // const text1 = bannerText1 || "To check your marksheet, Obtain your unique code from your instructor.";
   // const text2 = bannerText2 || "Enter the code to view your marksheet.";
@@ -39,9 +45,9 @@ export default function Banner({
   // Create the full message with link included
   const fullMessage = (
     <>
-      <a href={url}>{urlText}</a>. |🔴| {text1} Go to{" "}
+      {/* <a href={url}>{urlText}</a>. |🔴| {" "} */}
       <a href={url}>{urlText}</a>. |🔴| {text2} Go to{" "}
-      <a href={url}>{urlText}</a>. |🔴| {text3} Go to{" "}
+      {/* <a href={url}>{urlText}</a>. |🔴| {" "} */}
       <a href={url}>{urlText}</a>. |🔴| {text4} Go to{" "}
     </>
   );
