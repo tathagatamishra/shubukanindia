d498dc24d7dcfc48c6633542b31176a022f05daf
Tathagata Mishra <tathagatamishra007@gmail.com>
2025-09-30 12:30:33 +0530

    input ui

--- components/Exam/Instructor/LoginSignup/Login.jsx
+++ components/Exam/Instructor/LoginSignup/Login.jsx
@@ -9,10 +9,30 @@ export default function Login() {
     router.push(page);
   };
   const [userType, setUserType] = useState(null);
+  const [email, setEmail] = useState("");
 
   return (
-    <div className="OnlineExam corner-shape w-full flex flex-col justify-center items-center gap-4 p-[20px] border !rounded-[40px] shadow-md">
-      <div className="corner-shape w-full h-[200px] flex flex-col justify-center items-center gap-4 border !rounded-[40px] shadow-md"></div>
+    // <div className="OnlineExam corner-shape w-full flex flex-col justify-center items-center gap-4 p-[20px] border !rounded-[40px] shadow-md">
+    <div className="w-full flex flex-col justify-center items-center gap-4">
+      {/* <form className="corner-shape w-full h-fit flex flex-col justify-s items-start p-[16px] pb-[32px] border border-[#88807E] !rounded-[40px] bg-[#fff] shadow-md"> */}
+      <form className="OnlineExam corner-shape w-full h-fit p-[16px] pb-[32px] shadow-md flex flex-col justify-center items-center gap-4 border !rounded-[40px]">
+        <label
+          htmlFor="email"
+          className="font-[600] text-[14px] sm:text-[16px] text-[#334155]"
+        >
+          Email
+        </label>
+        <input
+          required
+          type="text"
+          name="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter your email id"
+          className="corner-shape border font-[600] text-[14px] sm:text-[16px] px-[10px] sm:px-[18px]"
+        />
+      </form>
 
       <ExamBtn
         text="Log in"
